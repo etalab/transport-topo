d250ec8c8a70b0f09d5c4dd07ce4a5e1c6bb7aa1
Tristram Gräbener <tristramg@gmail.com>
2019-10-24 15:07:41 +0200

    Generate topo tools id to avoid searching by labels

--- src/database_initializer.rs
+++ src/database_initializer.rs
@@ -1,36 +1,47 @@
 use crate::api_client::{
-    claim_item, ApiClient, ApiError, ObjectType, PropertyDataType,
+    claim_string, claim_item, ApiClient, ApiError, ObjectType, PropertyDataType,
 };
 use crate::client::{Config, Items, Properties};
 use anyhow::Error;
 use inflector::Inflector;
 
 // insert item if not already there, and return its id
-fn get_or_create_item(
+fn get_or_create_item<'a>(
     client: &ApiClient,
     label: &str,
     claims: &[json::JsonValue],
+    topo_id_id: impl Into<Option<&'a str>>,
 ) -> Result<String, Error> {
+    let mut claims = Vec::from(claims);
+    if let Some(id) = topo_id_id.into() {
+        claims.push(claim_string(id, &label.to_snake_case()))
+    };
+
     // for an item, we need to do a separate query to check if the item is already there
     let id = if let Some(id) = client.find_entity_id(label)? {
         log::info!("item \"{}\" already exists with id {}", label, id);
         id
     } else {
-        let id = client.create_item(label, claims)?;
+        let id = client.create_item(label, &claims)?;
         log::info!("creating item \"{}\" with id {}", label, id);
         id
     };
-    Ok(id.trim_start_matches('Q').parse()?)
+    Ok(id.to_owned())
 }
 
-fn get_or_create_property(
+fn get_or_create_property<'a>(
     client: &ApiClient,
     label: &str,
     prop_type: PropertyDataType,
+    topo_id_id: impl Into<Option<&'a str>>,
 ) -> Result<String, Error> {
+    let claims = match topo_id_id.into() {
+        Some(id) => vec![claim_string(id, &label.to_snake_case())],
+        None => vec![],
+    };
     // 2 properties cannot have the same label, so we just try to insert it
     // and get the id of the conflicting property if present
-    let r = client.create_object(ObjectType::Property(prop_type), label, &[]);
+    let r = client.create_object(ObjectType::Property(prop_type), label, &claims);
     if let Err(ApiError::PropertyAlreadyExists { label, id }) = r {
         log::info!("property \"{}\" already exists with id {}", label, id);
         Ok(id)
@@ -52,37 +63,40 @@ pub fn initial_populate(
         ..Default::default()
     })?;
 
-    let _moo = get_or_create_property(&client, "topo tools id", PropertyDataType::Item);
+    let topo_id = get_or_create_property(&client, "Topo tools id", PropertyDataType::String, None)?;
 
-    let producer_class = get_or_create_item(&client, "producer", &[])?;
-    let instance_of = get_or_create_property(&client, "instance of", PropertyDataType::String)?;
+    let producer_class = get_or_create_item(&client, "producer", &[], topo_id.as_str())?;
+    let instance_of = get_or_create_property(&client, "instance of", PropertyDataType::Item, topo_id.as_str())?;
     let config = Config {
         api_endpoint: api_endpoint.to_owned(),
         sparql_endpoint: sparql_endpoint.to_owned(),
         properties: Properties {
-            produced_by: get_or_create_property(&client, "produced by", PropertyDataType::String)?,
+            produced_by: get_or_create_property(&client, "Produced by", PropertyDataType::String, topo_id.as_str())?,
             instance_of: instance_of.clone(),
             physical_mode: get_or_create_property(
                 &client,
-                "physical mode",
+                "Physical mode",
                 PropertyDataType::String,
+                topo_id.as_str()
             )?,
             gtfs_short_name: get_or_create_property(
                 &client,
-                "gtfs short name",
+                "GTFS short name",
                 PropertyDataType::String,
+                topo_id.as_str(),
             )?,
             gtfs_long_name: get_or_create_property(
                 &client,
-                "gtfs long name",
+                "GTFS long name",
                 PropertyDataType::String,
+                topo_id.as_str(),
             )?,
-            gtfs_id: get_or_create_property(&client, "gtfs id", PropertyDataType::String)?,
+            gtfs_id: get_or_create_property(&client, "GTFS id", PropertyDataType::String, topo_id.as_str())?,
         },
         items: Items {
-            line: get_or_create_item(&client, "line", &[])?,
+            line: get_or_create_item(&client, "line", &[],topo_id.as_str())?,
             producer: producer_class.to_owned(),
-            bus: get_or_create_item(&client, "bus", &[])?,
+            bus: get_or_create_item(&client, "bus", &[], topo_id.as_str())?,
         },
     };
     if default_producer {
@@ -91,6 +105,7 @@ pub fn initial_populate(
             &client,
             "bob the bus mapper",
             &[claim_item(&instance_of, &producer_class)],
+            topo_id.as_str(),
         )?;
     }
     Ok(config)
