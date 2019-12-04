ed5b8451c95529a9acb9da6c389144f0b3f93e3e
antoine-de <antoine.desbordes@gmail.com>
2019-12-04 11:35:04 +0100

    add a app_id in the logs
    if the run are parallelized, it's easier to track them with an APP_ID

--- src/log.rs
+++ src/log.rs
@@ -1,7 +1,22 @@
+use std::io::Write;
+
 pub fn init() {
-    if std::env::var("RUST_LOG").is_err() {
+    let log_level = std::env::var("RUST_LOG");
+    let app_id = std::env::var("APP_ID");
+    if log_level.is_err() || app_id.is_ok() {
         let mut builder = pretty_env_logger::formatted_builder();
-        builder.filter(None, log::LevelFilter::Info);
+        if let Ok(app_id) = app_id {
+            // APP_ID is used to output an id while logging
+            builder.format(move |buf, record| {
+                writeln!(buf, "[{}] {} - {}", app_id, record.level(), record.args())
+            });
+        }
+
+        if let Ok(s) = std::env::var("RUST_LOG") {
+            builder.parse_filters(&s);
+        } else {
+            builder.filter(None, log::LevelFilter::Info);
+        }
         builder.init();
     } else {
         pretty_env_logger::init();
